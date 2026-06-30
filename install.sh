4189cd5b1a3e99892d8edba176b8028694d2362e
Feng <fengw518@gmail.com>
2026-06-30 22:04:14 +0800

    Release v0.8.2

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 
 REPO_URL="https://github.com/sellersheetai/sellersheet-skills.git"
 REPO_RAW="https://raw.githubusercontent.com/sellersheetai/sellersheet-skills/main"
-VERSION="0.8.1"
+VERSION="0.8.2"
 
 # ---------- args ----------
 TARGET=""
